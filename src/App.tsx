d30d39888d94bad3765ff9c12c1f18cd231b9ce3
Hpollard003 <hpollard003@gmail.com>
2022-03-09 15:14:46 -0500

    Style components

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import Grid from "@material-ui/core/Grid";
 import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
 import Badge from "@material-ui/core/Badge";
 //Style
-import { Wrapper } from "./App.styles";
+import { Wrapper, StyledButton } from "./App.styles";
+
 
 //Types
 export type CartItemType = {
@@ -26,10 +27,12 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 
 const App = () => {
+  const [cartOpen , setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[])
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
   console.log(data)
 
-  const getTotalItems = () => null
+  const getTotalItems = (items: CartItemType[]) => null;
 
   const handleAddToCart = (clickedItem: CartItemType) => null;
 
@@ -41,6 +44,13 @@ const App = () => {
 
   return(
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+       Cart  
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error"/>
+        <AddShoppingCart/>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
